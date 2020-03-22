ae9e18a49cc7fdffbe8f21c337b4e1bb31b03941
Dieter Mourisse <dieter.mourisse@howest.be>
2020-03-22 13:10:04 +0100

    Commiting start situation winkelmandje

--- Winkelmandje/MandjeList.cs
+++ Winkelmandje/MandjeList.cs
@@ -8,53 +8,5 @@ namespace Winkelmandje
 {
     public class MandjeList
     {
-        private List<Product> content;
-
-        public MandjeList()
-        {
-            this.content = new List<Product>();
-        }
-
-        public void AddProduct(Product p)
-        {
-            content.Add(p);
-        }
-
-        public decimal GetTotalPrice()
-        {
-            decimal price = 0;
-            foreach (Product p in content)
-            {
-                price += p.GetPrice();
-            }
-
-            return price;
-        }
-
-        public Product GetMostExpensiveItem()
-        {
-            Product most = content[0];
-            foreach (Product p in content)
-            {
-                if (p.GetPrice() > most.GetPrice())
-                {
-                    most = p;
-                }
-            }
-            return most;
-        }
-
-        public Product GetCheapestItem()
-        {
-            Product cheapest = content[0];
-            foreach (Product p in content)
-            {
-                if (p.GetPrice() < cheapest.GetPrice())
-                {
-                    cheapest = p;
-                }
-            }
-            return cheapest;
-        }
     }
 }
