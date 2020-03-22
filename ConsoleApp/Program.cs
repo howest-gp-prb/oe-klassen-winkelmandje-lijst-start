ec8094503175a59d2f92a37a3a6293630f96ee00
Dieter Mourisse <dieter.mourisse@howest.be>
2020-03-22 13:02:41 +0100

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Winkelmandje;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MandjeList mandje = new MandjeList();
+
+            /*
+             * Schrijf hier je testcode
+             */
+        }
+    }
+}
---
+++ Winkelmandje/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winkelmandje
+{
+    public class Product
+    {
+        private string name;
+        private decimal price;
+
+
+        public Product(string name, decimal price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public decimal GetPrice()
+        {
+            return price;
+        }
+    }
+}
